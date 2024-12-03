df73b18145aaba511cf134f6a03a67ef881a1e1c
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-03 23:24:50 +0800

    update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,15 +10,11 @@ export default function Banner() {
   const { user } = useContext(UserContext); 
   const navigate = useNavigate();
 
-  // Check if user is authenticated by looking at the user context
-  const isAuthenticated = user && user.id !== null;
-
   const handleClick = () => {
     if (user.isAdmin) {
       notyf.success(`Welcome, ${user.username}! Navigating to the movies page.`);
       navigate('/posts');
     } else {
-      notyf.success(`Welcome, ${user.username}! Navigating to the movies page.`);
       navigate('/posts');
     }
   };
@@ -28,12 +24,9 @@ export default function Banner() {
       <Col className="text-center py-5">
         <h1>Welcome to BuggyThoughts</h1>
          <p>Debugging life and code, one thought at a time. Join us for insights, tips, and tools to elevate your coding journey!</p>
-
-        {isAuthenticated && (
           <Button variant="danger" onClick={handleClick}>
             {user.isAdmin ? 'Check Dashboard' : 'Explore Insights'}
           </Button>
-        )}
       </Col>
     </Row>
   );
