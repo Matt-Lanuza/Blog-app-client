567e4742af3b11c0c55d4824ef80a0d0ab21ea2e
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-04 18:48:23 +0800

    fix gaps in appnavbar

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -11,32 +11,32 @@ export default function AppNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" id="app-navbar">
       <Container>
-        <Navbar.Brand href="/" id="logo-name">BuggyThoughts</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" id="logo-name">BuggyThoughts</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto d-flex align-items-center">
 
-          <Nav.Link as={Link} to="/" className="navbar-collapse">Home</Nav.Link>
+            <Nav.Link as={Link} to="/" className="navbar-collapse">Home</Nav.Link>
 
-          {!user.isAdmin && (
-            <>
-            <Nav.Link href="/posts" className="navbar-collapse">Posts</Nav.Link>
-            <Nav.Link href="/my-posts" className="navbar-collapse">{user.username}</Nav.Link>
-            </>
-          )}
+            {!user.isAdmin && (
+              <>
+                <Nav.Link as={Link} to="/posts" className="navbar-collapse">Posts</Nav.Link>
+                <Nav.Link as={Link} to="/my-posts" className="navbar-collapse">{user.username}</Nav.Link>
+              </>
+            )}
 
-          {user && user.isAdmin === true && (
-            <Nav.Link href="/posts" className="navbar-collapse">Dashboard</Nav.Link>
-          )}
+            {user && user.isAdmin === true && (
+              <Nav.Link as={Link} to="/posts" className="navbar-collapse">Dashboard</Nav.Link>
+            )}
 
-          {user && user.id !== null ? (
-            <Nav.Link href="/logout" className="navbar-collapse">Logout</Nav.Link>
-          ) : (
-            <>
-              <Nav.Link href="/login" className="navbar-collapse">Login</Nav.Link>
-              <Nav.Link href="/register" className="navbar-collapse">Register</Nav.Link>
-            </>
-          )}
+            {user && user.id !== null ? (
+              <Nav.Link as={Link} to="/logout" className="navbar-collapse">Logout</Nav.Link>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login" className="navbar-collapse">Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" className="navbar-collapse">Register</Nav.Link>
+              </>
+            )}
 
           </Nav>
         </Navbar.Collapse>
@@ -44,4 +44,3 @@ export default function AppNavbar() {
     </Navbar>
   );
 }
-
